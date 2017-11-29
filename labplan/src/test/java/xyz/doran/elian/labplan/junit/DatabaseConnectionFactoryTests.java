2d3e8d05f3214eb7e28953b90bab3facee52be86
Elian Doran <elinnum@gmail.com>
2017-11-29 13:10:25 +0200

    Testing: Execute query in DatabaseConnectionFactoryTests.testConnection()

--- labplan/src/test/java/xyz/doran/elian/labplan/junit/DatabaseConnectionFactoryTests.java
+++ labplan/src/test/java/xyz/doran/elian/labplan/junit/DatabaseConnectionFactoryTests.java
@@ -59,5 +59,6 @@ public class DatabaseConnectionFactoryTests {
 		Connection conn = DatabaseConnectionFactory.getConnection();
 		
 		PreparedStatement statement = conn.prepareStatement("SELECT * FROM `patients`");
+		statement.executeQuery();
 	}
 }
