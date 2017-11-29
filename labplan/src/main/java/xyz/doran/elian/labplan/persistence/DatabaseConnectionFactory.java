bf38daa75b4d17a4cd7ff6997f0f124f387da344
Elian Doran <elinnum@gmail.com>
2017-11-29 12:57:04 +0200

    Persistence: Implement ConfigurationFactory & configuration loading

---
+++ labplan/src/main/java/xyz/doran/elian/labplan/persistence/DatabaseConnectionFactory.java
@@ -0,0 +1,77 @@
+package xyz.doran.elian.labplan.persistence;
+
+import java.io.*;
+import java.util.Properties;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import com.mysql.cj.jdbc.Driver;
+
+public class DatabaseConnectionFactory {
+	private static Properties properties;
+	
+	private static String configFilePath;
+
+	public static String getConfigFilePath() {
+		return configFilePath;
+	}
+
+	public static void setConfigFilePath(String path) {
+		configFilePath = path;
+	}
+	
+	public static Connection getConnection() throws IOException, SQLException {
+		if (properties == null)
+			load();
+		
+		DriverManager.registerDriver(new Driver());
+		
+		return DriverManager.getConnection(
+				properties.getProperty("address"),
+				properties.getProperty("username"),
+				properties.getProperty("password"));
+	}
+	
+	public static void load() throws IOException {
+		FileInputStream in = null;
+		
+		try {
+			in = new FileInputStream(configFilePath);
+			properties = new Properties();
+			properties.load(in);
+			
+			if (!properties.containsKey("address") ||
+				properties.getProperty("address").length() == 0)
+				throw new RuntimeException("Database address not found in configuration file.");
+			
+			if (!properties.containsKey("username") ||
+				properties.getProperty("username").length() == 0)
+				throw new RuntimeException("User name not found in configuration file.");
+			
+			if (!properties.containsKey("password") ||
+				properties.getProperty("password").length() == 0)
+				throw new RuntimeException("Password not found in configuration file.");
+		} finally {
+			in.close();
+		}
+	}
+	
+	public static void save(String address, String userName, String password, String database)
+			throws IOException {
+		FileOutputStream out = null;
+		
+		try {
+			out = new FileOutputStream(configFilePath);
+			Properties props = new Properties();
+			
+			props.setProperty("address", address);
+			props.setProperty("username", userName);
+			props.setProperty("password", password);
+			props.setProperty("database", database);
+			
+			props.save(out, "LabPlan Database Connection Parameters");
+		} finally {
+			out.close();
+		}
+	}
+}
---
+++ labplan/src/test/java/xyz/doran/elian/labplan/junit/DatabaseConnectionFactoryTests.java
@@ -0,0 +1,64 @@
+package xyz.doran.elian.labplan.junit;
+
+import static org.junit.Assert.*;
+import static org.junit.Assume.assumeNoException;
+import static org.junit.Assume.assumeNotNull;
+import static org.junit.Assume.assumeTrue;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import xyz.doran.elian.labplan.persistence.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DatabaseConnectionFactoryTests {
+	static String configFilePath = "db.properties";
+	
+	static String dummyAddress = "jdbc:mysql://localhost:3306/labplan";
+	static String dummyUserName = "root";
+	static String dummyPassword = "Development.";
+	static String dummyDatabase = "database";
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testSaveConfiguration() {
+		try {
+			DatabaseConnectionFactory.setConfigFilePath(configFilePath);
+			DatabaseConnectionFactory.save(dummyAddress, dummyUserName, dummyPassword, dummyDatabase);
+		} catch (Exception e) {
+			assumeNoException(e);
+		}
+	}
+
+	@Test
+	public void testLoadConfiguration() {
+		try {
+			DatabaseConnectionFactory.setConfigFilePath(configFilePath);
+			DatabaseConnectionFactory.load();
+		} catch (Exception e) {
+			assumeNoException(e);
+		}
+	}
+	
+	@Test
+	public void testConnection() throws Exception {
+		DatabaseConnectionFactory.setConfigFilePath(configFilePath);
+		DatabaseConnectionFactory.load();
+		
+		Connection conn = DatabaseConnectionFactory.getConnection();
+		
+		PreparedStatement statement = conn.prepareStatement("SELECT * FROM `patients`");
+	}
+}
