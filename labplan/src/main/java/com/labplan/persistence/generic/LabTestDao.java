7a42bf27df5287959c6e8976b0b26dcf778a38d7
Elian Doran <elinnum@gmail.com>
2017-12-05 14:50:35 +0200

    Documentation: Add small docstrings to `LabTestDao` & `PatientDao`

--- labplan/src/main/java/com/labplan/persistence/generic/LabTestDao.java
+++ labplan/src/main/java/com/labplan/persistence/generic/LabTestDao.java
@@ -2,6 +2,12 @@ package com.labplan.persistence.generic;
 
 import com.labplan.entities.LabTest;
 
+/**
+ * An interface which implements a Data Access Object (DAO) with CRUD (Create, Read,
+ * Update, Delete) functionality for the {@link LabTest} entity.
+ * @author adoran
+ *
+ */
 public interface LabTestDao extends CrudInterface<LabTest, Integer> {
 
 }
--- labplan/src/main/java/com/labplan/persistence/generic/PatientDao.java
+++ labplan/src/main/java/com/labplan/persistence/generic/PatientDao.java
@@ -2,6 +2,18 @@ package com.labplan.persistence.generic;
 
 import com.labplan.entities.*;
 
+/**
+ * An interface which implements a Data Access Object (DAO) with CRUD (Create, Read,
+ * Update, Delete) functionality for the {@link Patient} entity.
+ * @author adoran
+ *
+ */
 public interface PatientDao extends CrudInterface<Patient, Integer> {
+	/**
+	 * Obtains a single {@link Patient} from a data source, searching by their name.
+	 * @param firstName	The first name of the patient.
+	 * @param lastName	The last name of the patient.
+	 * @return The {@link Patient} whose name matches.
+	 */
 	Patient read(String firstName, String lastName);
 }
