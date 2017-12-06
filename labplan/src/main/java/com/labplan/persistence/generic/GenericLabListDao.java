a779d3493d0a456aec1c466633b2e50b6774dff7
Elian Doran <elinnum@gmail.com>
2017-12-06 17:37:16 +0200

    Documentation: Add interface docstring for two generic DAOs

--- labplan/src/main/java/com/labplan/persistence/generic/GenericLabListDao.java
+++ labplan/src/main/java/com/labplan/persistence/generic/GenericLabListDao.java
@@ -1,7 +1,15 @@
 package com.labplan.persistence.generic;
 
 import com.labplan.entities.LabList;
+import com.labplan.entities.LabTest;
 
+/**
+ * An interface which implements a Data Access Object (DAO) with CRUD (Create,
+ * Read, Update, Delete) functionality for the {@link LabList} entity.
+ * 
+ * @author adoran
+ *
+ */
 public interface GenericLabListDao extends CrudInterface<LabList, Integer> {
 
 }
--- labplan/src/main/java/com/labplan/persistence/generic/GenericLabResultDao.java
+++ labplan/src/main/java/com/labplan/persistence/generic/GenericLabResultDao.java
@@ -6,6 +6,13 @@ import com.labplan.entities.LabTest;
 import com.labplan.entities.generic.CompositeKeyPair;
 import com.labplan.entities.generic.LazyLoadedEntity;
 
+/**
+ * An interface which implements a Data Access Object (DAO) with CRUD (Create,
+ * Read, Update, Delete) functionality for the {@link LabResult} entity.
+ * 
+ * @author adoran
+ *
+ */
 public interface GenericLabResultDao
 	extends CrudInterface<LabResult, CompositeKeyPair<
 										LazyLoadedEntity<Integer, LabTest>,
