ae2c035cc45558f165fa7f22fea5b8824bc4465e
Elian Doran <elinnum@gmail.com>
2017-12-08 14:56:02 +0200

    Documentation: Add docstrings to `GenericLabResultDao`

--- labplan/src/main/java/com/labplan/persistence/generic/GenericLabResultDao.java
+++ labplan/src/main/java/com/labplan/persistence/generic/GenericLabResultDao.java
@@ -8,8 +8,12 @@ import com.labplan.entities.LabTest;
 import com.labplan.entities.generic.LazyLoadedEntity;
 
 /**
- * An interface which implements a Data Access Object (DAO) with CRUD (Create,
- * Read, Update, Delete) functionality for the {@link LabResult} entity.
+ * An interface which implements a Data Access Object (DAO) with a parameterized
+ * CRUD (Create, Read, Update, Delete) functionality for the {@link LabResult} entity.
+ * 
+ * <p><em>The creation, reading, update and deletion of a {@link LabResult} is dependent on its parent {@link LabList}.
+ * Due to this, {@linkplain GenericLabResultDao} is not compatible with {@link CrudInterface}.
+ * </em></p>
  * 
  * @author adoran
  *
@@ -23,9 +27,33 @@ public interface GenericLabResultDao
 	 */
 	Set<LabResult> read(LabList list);
 	
+	/**
+	 * Inserts the given {@link LabResult} into the data source.
+	 * @param list		The parent {@link LabList} of this {@link LabResult}.
+	 * @param entity	The {@link LabResult} to be inserted.
+	 * @return The key of the {@link LabResult} after insertion, a {@link LazyLoadedEntity} containing the corresponding {@link LabTest}.
+	 */
 	LazyLoadedEntity<Integer, LabTest> create(LabList list, LabResult entity);
 	
+	/**
+	 * Updates an existing entity from the data source with the provided data. The
+	 * keys of both the entity from the data source and the given entity must be
+	 * identical for the update to work.
+	 * 
+	 * @param list		The parent {@link LabList} of this {@link LabResult}.
+	 * @param entity	The {@link LabResult} to be updated.
+	 * @return
+	 */
 	boolean update(LabList list, LabResult entity);
 	
+	/**
+	 * Removes an existing entry from the data source. Only the keys of the data
+	 * source entity and the given entity must match in order for the deletion to
+	 * work.
+	 * 
+	 * @param list		The parent {@link LabList} of this {@link LabResult}. 
+	 * @param entity	The {@link LabResult} to be deleted.
+	 * @return
+	 */
 	boolean delete(LabList list, LabResult entity);
 }
