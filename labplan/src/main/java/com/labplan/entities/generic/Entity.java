c4c0b81ea66df627870f93cdee9014c21ca7fe2e
Elian Doran <elinnum@gmail.com>
2017-12-08 12:53:17 +0200

    Documentation: Add docstrings for `Entity` & `LazyLoadedEntity`

--- labplan/src/main/java/com/labplan/entities/generic/Entity.java
+++ labplan/src/main/java/com/labplan/entities/generic/Entity.java
@@ -1,12 +1,31 @@
 package com.labplan.entities.generic;
 
+/**
+ * An <i>entity</i> represents a generic object holding information that is usually obtained from/inserted into a data source.
+ * The entity can be identified by a key of a generic type {@code TKey}.
+ * 
+ * <p>In the context of a data source that is a database, each useful table could be represented as distinct entities, while the
+ * key of the entity would actually be the table's primary key.</p>
+ * 
+ * @author adoran
+ *
+ * @param <TKey>	The type used by the key to identify this {@link Entity}.
+ */
 public abstract class Entity<TKey> {
 	protected TKey id;
 
+	/**
+	 * Gets the key used for identifying this {@link Entity}.
+	 * @return The key used for identifying this {@link Entity}.
+	 */
 	public TKey getId() {
 		return this.id;
 	}
 
+	/**
+	 * Sets the key used for identifying this {@link Entity}.
+	 * @param id The key to set.
+	 */
 	public void setId(TKey id) {
 		this.id = id;
 	}
--- labplan/src/main/java/com/labplan/entities/generic/LazyLoadedEntity.java
+++ labplan/src/main/java/com/labplan/entities/generic/LazyLoadedEntity.java
@@ -3,16 +3,36 @@ package com.labplan.entities.generic;
 import com.labplan.entities.LabList;
 import com.labplan.persistence.generic.CrudInterface;
 
+/**
+ * A {@link LazyLoadedEntity} is a wrapper around a {@link Entity} that delays the loading of the said entity until it is actually needed.
+ * This can be useful when eager loading of an entity in a list could generate a large amount of database traffic, without using a fraction of the loaded data.
+ * 
+ * <p>In order for the lazy loading to work, the {@link LazyLoadedEntity} stores the {@code Key} of the entity. When the entity is requested
+ * via {@code getEntity()}, if it was not previously loaded the {@link LazyLoadedEntity} will call the DAO stored in the {@code Dao} to
+ * obtain the entity. Any future calls will obtain the cached entity.</p>
+ * @author adoran
+ *
+ * @param <TKey>		The type of the key used by {@code TKey}.
+ * @param <TEntity>		The {@link Entity} wrapped around this {@link LazyLoadedEntity}.
+ */
 public class LazyLoadedEntity<TKey, TEntity extends Entity<TKey>> {
 	TEntity entity;
 	Boolean isLoaded;
 	CrudInterface<TEntity, TKey> dao;
 	TKey key;
 
+	/**
+	 * Create a new instance of {@link LazyLoadedEntity}. Its status is set to <i>not loaded</i>;
+	 * {@code setEntity()}, {@code setKey()} and {@code setDao()} must be used to make the lazy loading work.
+	 */
 	public LazyLoadedEntity() {
 		isLoaded = false;
 	}
 	
+	/**
+	 * Create a new instance of {@link LazyLoadedEntity} based around a 
+	 * @param entity
+	 */
 	public LazyLoadedEntity(TEntity entity) {
 		key = entity.getId();
 		this.entity = entity;
