20fde81b8c1ee4f8e184cb2a894d41773a88c5a6
Elian Doran <elinnum@gmail.com>
2017-12-08 14:09:51 +0200

    Console List: Move table printing into own class

--- labplan/src/main/java/com/labplan/console_list/ConsoleList.java
+++ labplan/src/main/java/com/labplan/console_list/ConsoleList.java
@@ -9,8 +9,6 @@ import java.util.Set;
 import com.labplan.entities.*;
 import com.labplan.persistence.sql.*;
 
-import io.bretty.console.table.*;
-
 /**
  * A small application that prints to the console in a tabular fashion all the {@link LabList} and their corresponding {@link LabResult}.
  * @author adoran
@@ -65,7 +63,7 @@ public class ConsoleList {
 			index++;
 		}
 		
-		displayListTable(ids, firstNames, lastNames, creationDates);
+		ListTablePrinting.displayListTable(ids, firstNames, lastNames, creationDates);
 	}
 	
 	/**
@@ -97,38 +95,6 @@ public class ConsoleList {
 			index++;
 		}
 		
-		displaySingleListTable(testNames, values, minimums, maximums);
-	}
-	
-	/**
-	 * Displays a table showing information about multiple {@link LabList}s.
-	 * @param ids				A {@link Number[]} containing the IDs of each {@link LabList}.
-	 * @param firstNames		A {@link String[]) containing the first names of the {@link Patient} of each {@link LabList}.
-	 * @param lastNames			A {@link String[]) containing the last names of the {@link Patient} of each {@link LabList}.
-	 * @param creationDates		A {@link Date[]} containing the creation dates of every {@link LabList}.
-	 */
-	private static void displayListTable(Number[] ids, String[] firstNames, String[] lastNames, Date[] creationDates) {
-		DateFormat df = DateFormat.getDateTimeInstance();
-		
-		Table.Builder builder = new Table.Builder("ID", ids, ColumnFormatter.number(Alignment.RIGHT, 5, Precision.ZERO));
-		builder.addColumn("First Name", firstNames, ColumnFormatter.text(Alignment.LEFT, 15));
-		builder.addColumn("Last Name", lastNames, ColumnFormatter.text(Alignment.LEFT, 15));
-		builder.addColumn("Creation date", creationDates, ColumnFormatter.dateTime(Alignment.LEFT, 20, df));
-		System.out.println(builder.build());
-	}
-	
-	/**
-	 * Displays a table showing information about a single {@link LabList}.
-	 * @param testNames			A {@link String[]} containing the name of the {@link LabTest} of every {@link LabResult}.
-	 * @param values			A {@link Number[]} containing the {@code value} of every {@link LabResult}.
-	 * @param minimums			A {@link Number[]} containing the {@code minimumValue} of the {@link LabTest} of every {@link LabResult}.
-	 * @param maximums			A {@link Number[]} containing the {@code maximumValue} of the {@link LabTest} of every {@link LabResult}.
-	 */
-	private static void displaySingleListTable(String[] testNames, Number[] values, Number[] minimums, Number[] maximums) {
-		Table.Builder builder = new Table.Builder("Test Name", testNames, ColumnFormatter.text(Alignment.LEFT, 40));
-		builder.addColumn("Min", minimums, ColumnFormatter.number(Alignment.LEFT, 5, Precision.TWO));
-		builder.addColumn("Max", maximums, ColumnFormatter.number(Alignment.LEFT, 5, Precision.TWO));
-		builder.addColumn("Value", values, ColumnFormatter.number(Alignment.LEFT, 5, Precision.TWO));
-		System.out.println(builder.build());
+		ListTablePrinting.displaySingleListTable(testNames, values, minimums, maximums);
 	}
 }
