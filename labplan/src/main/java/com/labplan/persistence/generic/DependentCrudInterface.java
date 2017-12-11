ed09206d6c794beb72b2d4b2ad4bce32fd7a7d55
Elian Doran <elinnum@gmail.com>
2017-12-11 15:15:22 +0200

    Documentation: Add docstrings for DependentCrudInterface & DependentCrudTester

--- labplan/src/main/java/com/labplan/persistence/generic/DependentCrudInterface.java
+++ labplan/src/main/java/com/labplan/persistence/generic/DependentCrudInterface.java
@@ -2,7 +2,24 @@ package com.labplan.persistence.generic;
 
 import com.labplan.entities.generic.Entity;
 
+/**
+ * An interface which describes a Data Access Object (DAO) which implements the
+ * Create, Read, Update and Delete (CRUD) functionality for a given entity.
+ * 
+ * <p>Unlike a {@link CrudInterface}, entities of a {@link DependentCrudInterface} have a parent {@link Entity}.
+ * These entities cannot exist outside the context of their parent entity.</p>
+ * 
+ * @author Elian Doran
+ *
+ * @param <TParentEntity>	A type which represents the parent entity of the {@code TChildEntity}.
+ * @param <TChildEntity>	A type which represents the actual entity, child of {@code TParentEntity}.
+ * @param <TChildKey>		The type which represents the key of the {@code TChildEntity}.
+ */
 public interface DependentCrudInterface<TParentEntity extends Entity<?>, TChildEntity extends Entity<TChildKey>, TChildKey>
 		extends CrudInterface<TChildEntity, TChildKey> {
+	/**
+	 * Obtains the parent entity of this entity.
+	 * @return The parent entity of this entity.
+	 */
 	public TParentEntity getParentEntity();
 }
--- labplan/src/test/java/com/labplan/helpers/DependentCrudTester.java
+++ labplan/src/test/java/com/labplan/helpers/DependentCrudTester.java
@@ -1,6 +1,20 @@
-package com.labplan.helpers;import com.labplan.entities.generic.Entity;
+package com.labplan.helpers;
+
+import com.labplan.entities.generic.Entity;
 import com.labplan.persistence.generic.DependentCrudInterface;
 
+/**
+ * This is an abstract test suite which implements a set of predefined test
+ * cases for testing the CRUD capabilities of a {@link DependentCrudInterface}
+ * implementation.
+ * 
+ * @author Elian Doran
+ *
+ * @param <TParent>	A type which represents the parent entity of the {@code TEntity}.
+ * @param <TKey>	The type which represents the key of the {@code TEntity}.
+ * @param <TEntity>	A type which represents the actual entity, child of {@code TParent}.
+ * @param <TDao>	A {@link DependentCrudInterface} representing the DAO for {@code TEntity}.
+ */
 public abstract class DependentCrudTester<TParent extends Entity<?>, TKey, TEntity extends Entity<TKey>, TDao extends DependentCrudInterface<TParent, TEntity, TKey>>
 	extends CrudTester<TKey, TEntity, TDao> {
 	
