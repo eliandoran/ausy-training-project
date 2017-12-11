89331b5225b9429f94faeb2b26b8c74cb200e9e7
Elian Doran <elinnum@gmail.com>
2017-12-11 17:27:37 +0200

    Documentation: Add docstrings to `TestMessages`

--- labplan/src/test/java/com/labplan/helpers/TestMessages.java
+++ labplan/src/test/java/com/labplan/helpers/TestMessages.java
@@ -1,10 +1,43 @@
 package com.labplan.helpers;
 
+/**
+ * Contains a set of constant string messages which can be used for displaying
+ * unit testing errors related to common CRUD operations like the ones defined
+ * in {@link DaoTester}.
+ * 
+ * @author Elian Doran
+ *
+ */
 public final class TestMessages {
+	/**
+	 * Message to be displayed when a {@link Dao} READ of a previously inserted
+	 * entity fails.
+	 */
 	public static final String MSG_ENTITY_NOT_FOUND = "Inserted entity not found";
+
+	/**
+	 * Message to be displayed when the generated entity and the one retrieved from
+	 * the data source are not identical.
+	 */
 	public static final String MSG_ENTITIES_NOT_IDENTICAL = "Dummy entity and retrieved entity are not identical.";
+
+	/**
+	 * Message to be displayed when a {@link Dao} DELETE fails.
+	 */
 	public static final String MSG_DELETION_FAILED = "SQL deletion for single entity failed.";
+
+	/**
+	 * Message to be displayed when a {@link Dao} UPDATE fails.
+	 */
 	public static final String MSG_UPDATE_FAILED = "SQL update for single entity failed.";
+
+	/**
+	 * Message to be displayed when a {@link Dao} READ fails.
+	 */
 	public static final String MSG_RETRIEVAL_FAILED = "SQL retrieval for single entity failed.";
+
+	/**
+	 * Message to be displayed when a {@link Dao} INSERT fails.
+	 */
 	public static final String MSG_INSERTION_FAILED = "SQL insertion failed.";
 }
