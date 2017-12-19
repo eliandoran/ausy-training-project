e4a7796cff5743d3543ae64eab599bc54c9d5d99
Elian Doran <elinnum@gmail.com>
2017-12-19 23:14:14 +0200

    Use a combobox for selecting patients in Add Lab list

--- labplan/src/main/java/com/labplan/webapp/handlers/lab_list/AddLabListResourceHandler.java
+++ labplan/src/main/java/com/labplan/webapp/handlers/lab_list/AddLabListResourceHandler.java
@@ -2,12 +2,17 @@ package com.labplan.webapp.handlers.lab_list;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
+import com.labplan.entities.Patient;
+import com.labplan.persistence.generic.PatientDao;
 import com.labplan.persistence.sql.SqlLabListDao;
+import com.labplan.persistence.sql.SqlPatientDao;
 import com.labplan.services.LabListService;
 import com.labplan.webapp.HandlerParameters;
 import com.labplan.webapp.ResourceHandler;
@@ -31,6 +36,7 @@ public class AddLabListResourceHandler implements ResourceHandler {
 		HttpServletRequest request = params.getRequest();
 		request.setAttribute("creation_date", new Date());
 		request.setAttribute("is_new", true);
+		request.setAttribute("patients", getPatients());
 		
 		RequestDispatcher dispatcher = params.getContext().getRequestDispatcher("/app/lists/add.jsp");
 		dispatcher.forward(params.getRequest(), params.getResponse());
@@ -41,5 +47,13 @@ public class AddLabListResourceHandler implements ResourceHandler {
 	public boolean doPost(HandlerParameters params) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		return false;
-	}		
+	}
+	
+	private List<Patient> getPatients() {
+		PatientDao patientDao = new SqlPatientDao();		
+		List<Patient> patientsList = new LinkedList<Patient>();
+		patientsList.addAll(patientDao.readAll());
+		
+		return patientsList;
+	}
 }
