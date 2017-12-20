b345d03427d8a6817ecee8e7e82f8bdb6481935d
Elian Doran <elinnum@gmail.com>
2017-12-20 17:35:11 +0200

    Edit lab list data entry: Fix insertion on empty field

--- labplan/src/main/webapp/assets/lab_list.js
+++ labplan/src/main/webapp/assets/lab_list.js
@@ -1,7 +1,14 @@
 var resultsTable = document.querySelector("#results-table");
 var templateRow = document.querySelector("#results-template");
 
-templateRow.querySelector(".value").onchange = function() {
+templateRow.querySelector(".value").onchange = newRow;
+templateRow.querySelector(".type").onchange = newRow;
+
+function newRow() {
+	if (templateRow.querySelector(".type").value == "" ||
+		templateRow.querySelector(".value").value == "")
+		return;
+	
 	var templateClone = templateRow.cloneNode(true);
 	templateClone.id = templateClone.className = "";
 	resultsTable.querySelector("tbody").append(templateClone);
@@ -12,4 +19,4 @@ templateRow.querySelector(".value").onchange = function() {
 	templateRow.querySelector(".value").value = "";
 	
 	console.log("Changed!");
-};
+}
