52897582fb32de7757192592b98c64786b78b2f5
Elian Doran <elinnum@gmail.com>
2017-12-22 15:05:01 +0200

    Documentation: Add docstrings for Pair class

--- labplan/src/main/java/com/labplan/helpers/Pair.java
+++ labplan/src/main/java/com/labplan/helpers/Pair.java
@@ -1,41 +1,54 @@
 package com.labplan.helpers;
 
+/**
+ * A pair represents a tuple of exactly two elements. 
+ * @author Elian Doran
+ *
+ * @param <TFirst>		The type of the first element of the pair.
+ * @param <TSecond>		The type of the second element of the pair.
+ */
 public class Pair<TFirst, TSecond> {
 	private TFirst first;
 	private TSecond second;
 	
-	public Pair() {
-		
-	}
+	/**
+	 * Creates a new empty pair.
+	 */
+	public Pair() { }
 	
+	/**
+	 * Creates a pair with the given elements.
+	 * @param first		The first element of the pair.
+	 * @param second	The second element of the pair.
+	 */
 	public Pair(TFirst first, TSecond second) {
 		this.setFirst(first);
 		this.setSecond(second);
 	}
 
 	/**
-	 * @return the first
+	 * @return The first element of the pair.
 	 */
 	public TFirst getFirst() {
 		return first;
 	}
 
 	/**
-	 * @param first the first to set
+	 * @param first The first element of the pair.
 	 */
 	public void setFirst(TFirst first) {
 		this.first = first;
 	}
 
 	/**
-	 * @return the second
+	 * @return The second element of the pair.
 	 */
 	public TSecond getSecond() {
 		return second;
 	}
 
 	/**
-	 * @param second the second to set
+	 * @param The second element of the pair.
 	 */
 	public void setSecond(TSecond second) {
 		this.second = second;
