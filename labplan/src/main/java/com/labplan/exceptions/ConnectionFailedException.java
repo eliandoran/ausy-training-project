054825a713b327d1d1b6557666dfc58a19c4451e
Elian Doran <elinnum@gmail.com>
2017-12-22 15:34:07 +0200

    Documentation: Add docstrings for ConnectionFailedException & ValidationException

--- labplan/src/main/java/com/labplan/exceptions/ConnectionFailedException.java
+++ labplan/src/main/java/com/labplan/exceptions/ConnectionFailedException.java
@@ -1,10 +1,26 @@
 package com.labplan.exceptions;
 
+/**
+ * Thrown when a connection to the data source cannot be obtained, either due to misconfiguration of authentication
+ * data or a problem with the server.
+ * 
+ * <p>It is recommended to use {@link ConnectionFailedException} as a wrapper to an underlying error (i.e. SQL connection
+ * error) by using {@link ConnectionFailedException#addSuppressed(Throwable)}.</p>
+ * @author Elian Doran
+ *
+ */
 public class ConnectionFailedException extends RuntimeException {
+	/**
+	 * Creates a new instance of the exception without specifying a message.
+	 */
 	public ConnectionFailedException() {
 
 	}
 
+	/**
+	 * Creates a new instance of the exception with the given {@code message}.
+	 * @param message
+	 */
 	public ConnectionFailedException(String message) {
 		super(message);
 	}
--- labplan/src/main/java/com/labplan/exceptions/ValidationException.java
+++ labplan/src/main/java/com/labplan/exceptions/ValidationException.java
@@ -3,23 +3,48 @@ package com.labplan.exceptions;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * Represents an exception that is generated when a user enters invalid data in a {@code <form>}.
+ * Because validating a single field at once might be undesirable for a user, this exception is
+ * actually a collection of errors for each invalid field.
+ * @author Elian Doran
+ *
+ */
 public class ValidationException extends RuntimeException {
 	private static final long serialVersionUID = 5963249023483958037L;
 
 	private Map<String, String> fields;
 
+	/**
+	 * Creates a new, empty {@link ValidationException}.
+	 */
 	public ValidationException() {
 		fields = new LinkedHashMap<>();
 	}
 
+	/**
+	 * Adds a new error with the for the given {@code fieldName}. Only one {@code message} can exist
+	 * per {@code fieldName}.
+	 * @param fieldName	A name representing the {@code <form>} field (i.e. <em>first name</em>).
+	 * @param message	The message describing the validation error for the given {@code fieldName}.
+	 */
 	public void addField(String fieldName, String message) {
 		fields.put(fieldName, message);
 	}
 
+	/**
+	 * Removes any error message associated with the given {@code fieldName}.
+	 * @param fieldName	The name of the field whose error message to remove.
+	 */
 	public void removeField(String fieldName) {
 		fields.remove(fieldName);
 	}
 
+	/**
+	 * Returns a {@link Map} whose <em>keys</em> represent the name of the fields and whose <em>values</em> represent
+	 * the error message for the corresponding key.
+	 * @return	A {@link Map} of all the fields and the corresponding error messages.
+	 */
 	public Map<String, String> getFields() {
 		return fields;
 	}
