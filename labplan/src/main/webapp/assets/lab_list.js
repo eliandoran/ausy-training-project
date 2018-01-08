b3cf6994f603074c25610ba0f5bcffed052f69fb
Elian Doran <elinnum@gmail.com>
2018-01-08 15:57:41 +0200

    Reposition text box cursor properly in Edit Lab List

--- labplan/src/main/webapp/assets/lab_list.js
+++ labplan/src/main/webapp/assets/lab_list.js
@@ -31,6 +31,17 @@ function newRow() {
 	templateRowType.value = "";
 	templateRowValue.value = "";
 	
-	templateCloneValue.focus();
+	moveCaretToEnd(templateCloneValue);
 	templateCloneValue.scrollIntoView();
+}
+
+function moveCaretToEnd(el) {
+    el.focus();
+    if (typeof el.selectionStart == "number") {
+        el.selectionStart = el.selectionEnd = el.value.length;
+    } else if (typeof el.createTextRange != "undefined") {
+        var range = el.createTextRange();
+        range.collapse(false);
+        range.select();
+    }
 }
