e47d5b8955fbd1a51ae3f8dc8838221ee615e7bf
Elian Doran <elinnum@gmail.com>
2018-01-12 18:08:15 +0200

    Set up JSON response & return all patients

--- labplan/src/main/java/com/labplan/api/PatientsController.java
+++ labplan/src/main/java/com/labplan/api/PatientsController.java
@@ -1,17 +1,33 @@
 package com.labplan.api;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+import com.labplan.entities.Patient;
+import com.labplan.persistence.DatabaseConnectionFactory;
 import com.labplan.persistence.generic.PatientDao;
 import com.labplan.persistence.sql.SqlPatientDao;
 
 @Path("/patients")
 public class PatientsController {
-	private static PatientDao patientDao = new SqlPatientDao();
+	private static final PatientDao patientDao = new SqlPatientDao();
+	
+	public PatientsController() {
+		DatabaseConnectionFactory.setProfile("production");
+	}
 	
 	@GET
-	public String getPatients() {
-		return "patients";
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Patient> getPatients() {
+		Set<Patient> patientSet = patientDao.readAll();
+		List<Patient> patientList = new LinkedList<>();
+		patientList.addAll(patientSet);
+		return patientList;
 	}
 }
