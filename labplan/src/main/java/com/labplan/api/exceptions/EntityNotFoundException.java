a5f5f573b88dad01c254ba5eb3163edf7ef9242d
Elian Doran <elinnum@gmail.com>
2018-01-15 17:35:51 +0200

    Add documentation to EntityNotFoundException class

--- labplan/src/main/java/com/labplan/api/exceptions/EntityNotFoundException.java
+++ labplan/src/main/java/com/labplan/api/exceptions/EntityNotFoundException.java
@@ -1,5 +1,12 @@
 package com.labplan.api.exceptions;
 
+/**
+ * A {@link RuntimeException} indicating that a request to get an entity (for example, by ID) failed because no corresponding entity
+ * could be found.
+ * @author Elian Doran
+ * @see com.labplan.entities.Patient
+ * @see com.labplan.api.PatientsController
+ */
 public class EntityNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = -7343683268510879223L;
 	
