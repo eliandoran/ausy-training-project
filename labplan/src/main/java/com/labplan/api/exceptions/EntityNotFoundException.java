142c507e1e55508978eaa27d2d72740b32fdd579
Elian Doran <elinnum@gmail.com>
2018-01-15 16:28:46 +0200

    API: Return JSON error response instead of default HTML response

--- labplan/src/main/java/com/labplan/api/exceptions/EntityNotFoundException.java
+++ labplan/src/main/java/com/labplan/api/exceptions/EntityNotFoundException.java
@@ -2,4 +2,9 @@ package com.labplan.api.exceptions;
 
 public class EntityNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = -7343683268510879223L;
+	
+	@Override
+	public String getMessage() {
+		return "The entity could not be found.";
+	}
 }
--- labplan/src/main/java/com/labplan/api/providers/EntityNotFoundHandler.java
+++ labplan/src/main/java/com/labplan/api/providers/EntityNotFoundHandler.java
@@ -5,12 +5,19 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.json.JSONObject;
+
 import com.labplan.api.exceptions.EntityNotFoundException;
 
 @Provider
 public class EntityNotFoundHandler implements ExceptionMapper<EntityNotFoundException> {
 	@Override
 	public Response toResponse(EntityNotFoundException exception) {
-		return Response.status(Status.NOT_FOUND).entity(exception.getMessage()).build();
+		JSONObject response = new JSONObject();
+		response.put("status", "error");
+		response.put("error", Status.NOT_FOUND);
+		response.put("message", exception.getMessage());
+		
+		return Response.status(Status.NOT_FOUND).entity(response.toString()).build();
 	}
 }
